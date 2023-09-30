6866e9dd47b0ae1c8a74c8538ee53484d0a3fd19
Chamodh Abeygoonawardena <it20189044@my.sliit.lk>
2023-09-30 23:12:36 +0530

    Fragement development completed

--- mobile/app/src/main/java/com/example/mobile_app/HomeFragement.java
+++ mobile/app/src/main/java/com/example/mobile_app/HomeFragement.java
@@ -10,13 +10,14 @@ import android.view.ViewGroup;
 
 import com.example.mobile_app.adapters.TrainScheduleAdapter;
 import com.example.mobile_app.databinding.ActivityHomeBinding;
+import com.example.mobile_app.databinding.FragmentHomeFragementBinding;
 import com.example.mobile_app.models.TrainSchedule;
 
 import java.util.ArrayList;
 
 public class HomeFragement extends Fragment {
 
-    ActivityHomeBinding binding;
+    FragmentHomeFragementBinding binding;
     TrainScheduleAdapter listAdapter;
     ArrayList<TrainSchedule> dataList = new ArrayList<>();
     TrainSchedule trainSchedule;
@@ -25,8 +26,28 @@ public class HomeFragement extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        View view = inflater.inflate(R.layout.fragment_home_fragement, container, false);
+        binding = FragmentHomeFragementBinding.inflate(inflater, container, false);
+        View view = binding.getRoot();
 
+        trainSchedule = new TrainSchedule("1", "Sudu Manika", "ML-2020", "Akalanka", "0778899483", "Gampaha", "Galle", "7.30", "2.30", 8, 3, 200);
+        dataList.add(trainSchedule);
+        trainSchedule = new TrainSchedule("1", "Kalu Manika", "ML-2030", "Danushka", "077884935", "Seeduwa", "Negombo", "7.30", "2.30", 8, 3, 200);
+        dataList.add(trainSchedule);
+
+        listAdapter = new TrainScheduleAdapter(requireContext(), dataList);
+        binding.listView.setAdapter(listAdapter);
+//        binding.listView.setClickable(true);
+
+//        binding.listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+//                Intent intent = new Intent(HomeActivity.this, DetailedActivity.class);
+//                intent.putExtra("trainName", "Sudu Manika");
+//                intent.putExtra("model", "MP-2020");
+//                startActivity(intent);
+//
+//            }
+//        });
         return view;
     }
 }
