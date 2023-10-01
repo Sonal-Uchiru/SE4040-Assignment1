6c7646e11e905eb92392364f8922275304eb8ded
Kaveen_Wijethunga <it20185770@my.sliit.lk>
2023-10-01 14:36:34 +0530

    View train schedule modal ui completed

---
+++ web/src/components/modals/train/ViewTrainScheduleModal.tsx
@@ -0,0 +1,147 @@
+import * as React from "react";
+import { Formik, Form } from "formik";
+import Backdrop from "@mui/material/Backdrop";
+import Box from "@mui/material/Box";
+import Modal from "@mui/material/Modal";
+import Fade from "@mui/material/Fade";
+import theme from "../../../theme/hooks/CreateTheme";
+import Avatar from "@mui/material/Avatar";
+import ContainedButton from "../../atoms/buttons/ContainedButton";
+
+import HeadLine4 from "../../atoms/typographies/HeadLine4";
+import SelectField from "../../atoms/selectField/SelectFieldAtom";
+import CategoryProtectedApi from "../../../api/exclusive/CategoryProtectedApi";
+import {
+  ISelectField,
+  SelectFieldOptions,
+  getSelectFieldOptions,
+} from "../../../types/selectFields/SelectFieldTypes";
+import { AxiosError } from "axios";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import TrainScheduleModalTable from "../../organisms/tables/TrainScheduleModalTable";
+
+interface IProps {
+  handleCancel(): void;
+}
+
+export default function ViewTrainScheduleModal({ handleCancel }: IProps) {
+  return (
+    <>
+      <Modal
+        aria-labelledby="transition-modal-title"
+        aria-describedby="transition-modal-description"
+        open={true}
+        closeAfterTransition
+        slots={{ backdrop: Backdrop }}
+        slotProps={{
+          backdrop: {
+            timeout: 500,
+          },
+        }}
+      >
+        <Fade in={true}>
+          <Box sx={styles.box}>
+            <div style={{ textAlign: "center", marginTop: -15 }}>
+              <HeadLine4
+                text={"Train Schedules"}
+                color={theme.palette.primary.main}
+              />
+            </div>
+            <div style={styles.images}>
+              <Avatar
+                alt="Research Image"
+                src="./images/journey.png"
+                style={{
+                  marginTop: 20,
+                  width: 100,
+                  height: 100,
+                  objectFit: "contain",
+                }}
+                variant="rounded"
+              />
+            </div>
+            <div>
+              <TrainScheduleModalTable />
+            </div>
+            <div
+              style={styles.button}
+              className="btn-toolbar"
+              role="toolbar"
+              aria-label="Toolbar with button groups"
+            >
+              <div
+                className="btn-group"
+                role="group"
+                aria-label="Third group"
+                style={styles.buttonGroup2}
+              >
+                <ContainedButton
+                  title={"Cancel"}
+                  color={theme.palette.white.main}
+                  backgroundColor={theme.palette.primary.main}
+                  onClick={handleCancel}
+                  width={100}
+                />
+              </div>
+            </div>
+          </Box>
+        </Fade>
+      </Modal>
+    </>
+  );
+}
+
+const styles = {
+  box: {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    overFlow: "scroll",
+    transform: "translate(-50%, -50%)",
+    bgcolor: theme.palette.white.main,
+    boxShadow: 24,
+    p: 4,
+    borderRadius: 2,
+    overFlowY: "scroll",
+    maxWidth: 900,
+    maxHeight: 670,
+  },
+
+  button: {
+    marginTop: 20,
+    justifyContent: "center",
+    alignSelf: "center",
+  },
+
+  buttonGroup2: {
+    marginRight: 10,
+    marginLeft: 10,
+    marginTop: 10,
+  },
+
+  buttonGroup: {
+    marginRight: 10,
+    marginLeft: 10,
+    marginTop: 10,
+  },
+
+  images: {
+    justifyContent: "center",
+    alignSelf: "center",
+    display: "flex",
+    marginBottom: 10,
+  },
+
+  input: {
+    marginTop: 40,
+    marginBottom: 20,
+    display: "flex",
+    justifyContent: "center",
+  },
+};
