9315b7d6bc8883813401b6795e73eba97dcffd16
Chamodh Abeygoonawardena <it20189044@my.sliit.lk>
2023-10-01 11:50:38 +0530

    Dialog display fully completed

--- mobile/app/src/main/java/com/example/mobile_app/HomeFragement.java
+++ mobile/app/src/main/java/com/example/mobile_app/HomeFragement.java
@@ -1,17 +1,25 @@
 package com.example.mobile_app;
 
+import android.app.Dialog;
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.Button;
 
 import com.example.mobile_app.adapters.TrainScheduleAdapter;
 import com.example.mobile_app.databinding.ActivityHomeBinding;
 import com.example.mobile_app.databinding.FragmentHomeFragementBinding;
 import com.example.mobile_app.models.TrainSchedule;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.ArrayList;
 
@@ -21,14 +29,22 @@ public class HomeFragement extends Fragment {
     TrainScheduleAdapter listAdapter;
     ArrayList<TrainSchedule> dataList = new ArrayList<>();
     TrainSchedule trainSchedule;
+    FloatingActionButton bottomSheetBtn;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
         binding = FragmentHomeFragementBinding.inflate(inflater, container, false);
+        bottomSheetBtn = binding.btnDialogOpen;
         View view = binding.getRoot();
 
+        bottomSheetBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showDialog();
+            }
+        });
         trainSchedule = new TrainSchedule("1", "Sudu Manika", "ML-2020", "Akalanka", "0778899483", "Gampaha", "Galle", "7.30", "2.30", 8, 3, 200);
         dataList.add(trainSchedule);
         trainSchedule = new TrainSchedule("1", "Kalu Manika", "ML-2030", "Danushka", "077884935", "Seeduwa", "Negombo", "7.30", "2.30", 8, 3, 200);
@@ -65,5 +81,19 @@ public class HomeFragement extends Fragment {
 //            }
 //        });
         return view;
+    }
+
+    private void showDialog() {
+        Context context = requireContext();
+
+        final Dialog dialog = new Dialog(context);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(R.layout.search_bottom_sheet);
+
+        dialog.show();
+        dialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable((Color.TRANSPARENT)));
+        dialog.getWindow().getAttributes().windowAnimations = R.style.dialoglAnumation;
+        dialog.getWindow().setGravity(Gravity.BOTTOM);
     }
 }
