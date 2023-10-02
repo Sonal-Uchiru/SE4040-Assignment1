9f13f3b83ae8c2e1811c01b1915d415760c95367
Chamodh Abeygoonawardena <it20189044@my.sliit.lk>
2023-10-02 18:28:54 +0530

    Gradle updated

--- mobile/app/build.gradle.kts
+++ mobile/app/build.gradle.kts
@@ -36,7 +36,6 @@ android {
 }
 
 dependencies {
-
     implementation("androidx.core:core-ktx:1.9.0")
     implementation ("com.google.code.gson:gson:2.8.7")
     implementation("androidx.appcompat:appcompat:1.6.1")
@@ -48,6 +47,4 @@ dependencies {
 
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
-
-
 }
