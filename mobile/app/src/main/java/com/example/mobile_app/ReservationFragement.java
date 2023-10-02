116a3f603f9a65811bf75f3d6a89ffe8d3da05ce
Chamodh Abeygoonawardena <it20189044@my.sliit.lk>
2023-10-02 18:50:45 +0530

    UI files created

---
+++ mobile/app/src/main/java/com/example/mobile_app/ReservationFragement.java
@@ -0,0 +1,23 @@
+package com.example.mobile_app;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link ReservationFragement#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class ReservationFragement extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_reservation_fragement, container, false);
+    }
+}
